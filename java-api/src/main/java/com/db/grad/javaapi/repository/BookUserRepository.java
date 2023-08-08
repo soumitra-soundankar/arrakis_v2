880987d8d8da67d3a904eaa6cbe02fd1a1d5f1bc
vatsalkumar24 <83907433+vatsalkumar24@users.noreply.github.com>
2023-08-09 03:39:38 +0530

    Update BookUserRepository.java

--- java-api/src/main/java/com/db/grad/javaapi/repository/BookUserRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/BookUserRepository.java
@@ -1,11 +1,10 @@
 package com.db.grad.javaapi.repository;
 
 import com.db.grad.javaapi.model.BookUser;
-import com.db.grad.javaapi.model.BookUserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookUserRepository extends JpaRepository<BookUser, BookUserId> {
+public interface BookUserRepository extends JpaRepository<BookUser, Integer> {
     // You can add custom query methods here if needed
 }
